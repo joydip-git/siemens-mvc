fffc9069e6766d135f57583c67ac469d987c9804
siemens-c2c <joy_mon78@yahoo.co.in>
2019-09-23 12:04:38 +0530

    completed business layer

---
+++ PMSAPP/PMSAPP.BusinessLogicLayer/Implementation/ProductBusinessComponent.cs
@@ -0,0 +1,84 @@
+﻿using PMSAPP.BusinessLogicLayer.Abstract;
+using PMSAPP.DataAccessLayer.Abstract;
+using PMSAPP.DataAccessLayer.Implementation;
+using PMSAPP.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace PMSAPP.BusinessLogicLayer.Implementation
+{
+    public class ProductBusinessComponent : IBusinessComponent<Product>
+    {
+        private IDataAccess<Product> dataAccessObject;
+
+        public ProductBusinessComponent()
+        {
+            dataAccessObject = new ProductDataAccessComponent();
+        }
+        public int Add(Product data)
+        {
+            try
+            {
+                if (data != null)
+                    return dataAccessObject.InsertRecord(data);
+                else
+                    throw new ArgumentException("no data was passed");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public Product Fetch(int id)
+        {
+            try
+            {
+                return dataAccessObject.GetData(id);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public IEnumerable<Product> FetchAll()
+        {
+            try
+            {
+                return dataAccessObject.GetAll();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int Remove(int id)
+        {
+            try
+            {
+                return dataAccessObject.DeleteRecord(id);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int Update(Product data)
+        {
+            try
+            {
+                if (data != null)
+                    return dataAccessObject.UpdateRecord(data);
+                else
+                    throw new ArgumentException("no data was passed");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
