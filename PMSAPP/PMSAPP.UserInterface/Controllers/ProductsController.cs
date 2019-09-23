c582f86841bd0c8d4e0278d15dd2efd48fa3782d
siemens-c2c <joy_mon78@yahoo.co.in>
2019-09-23 12:29:50 +0530

    UI updated with a simple menu

---
+++ PMSAPP/PMSAPP.UserInterface/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PMSAPP.UserInterface.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string ShowProducts()
+        {
+            return "all products";
+        }
+    }
+}
