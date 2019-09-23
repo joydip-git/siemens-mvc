10138b853a98928ac05f14544efdee2a3c263a9a
siemens-c2c <joy_mon78@yahoo.co.in>
2019-09-23 11:58:22 +0530

    created interface for business layer

---
+++ PMSAPP/PMSAPP.BusinessLogicLayer/Abstract/IBusinessComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace PMSAPP.BusinessLogicLayer.Abstract
+{
+    public interface IBusinessComponent<T> where T : class
+    {
+        int Add(T data);
+        int Remove(int id);
+        int Update(T data);
+        IEnumerable<T> FetchAll();
+        T Fetch(int id);
+    }
+}
