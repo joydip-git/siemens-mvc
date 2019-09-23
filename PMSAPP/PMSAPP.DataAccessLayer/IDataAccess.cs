bc2ed88010e8933194f4f52fa839d0879b898f03
siemens-c2c <joy_mon78@yahoo.co.in>
2019-09-23 11:37:48 +0530

    created interface for Dal

---
+++ PMSAPP/PMSAPP.DataAccessLayer/IDataAccess.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace PMSAPP.DataAccessLayer
+{
+    public interface IDataAccess<T> where T : class
+    {
+        int InsertRecord(T data);
+        int DeleteRecord(int id);
+        int UpdateRecord(T data);
+        IEnumerable<T> GetAll();
+        T GetData(int id);
+    }
+}
