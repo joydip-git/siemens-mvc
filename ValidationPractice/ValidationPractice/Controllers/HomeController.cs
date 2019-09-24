2f0acb045abbdd283ebc851e028723fb59ee48ac
siemens-c2c <joy_mon78@yahoo.co.in>
2019-09-24 10:26:53 +0530

    Validation example application created

---
+++ ValidationPractice/ValidationPractice/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ValidationPractice.Models;
+
+namespace ValidationPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult MakeBooking()
+        {
+            return View(new Appointment { Date = DateTime.Now });
+        }
+        [HttpPost]
+        public ActionResult MakeBooking(Appointment appointment)
+        {
+            //
+            return View("Completed", appointment);
+        }
+    }
+}
