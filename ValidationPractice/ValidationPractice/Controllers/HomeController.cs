a7ac9cdddfe7e9161968f752d72269cd9f51d887
siemens-c2c <joy_mon78@yahoo.co.in>
2019-09-24 10:40:50 +0530

    updated with server side validation

--- ValidationPractice/ValidationPractice/Controllers/HomeController.cs
+++ ValidationPractice/ValidationPractice/Controllers/HomeController.cs
@@ -18,8 +18,23 @@ namespace ValidationPractice.Controllers
         [HttpPost]
         public ActionResult MakeBooking(Appointment appointment)
         {
-            //
-            return View("Completed", appointment);
+            if (string.IsNullOrEmpty(appointment.ClientName))
+            {
+                this.ModelState.AddModelError(nameof(appointment.ClientName), "Please enter your name");
+            }
+            if (ModelState.IsValidField(nameof(appointment.Date)) && DateTime.Now > appointment.Date)
+            {
+                ModelState.AddModelError(nameof(appointment.Date), "Please enter a future date");
+            }
+            if (!appointment.TermsAccepted)
+            {
+                ModelState.AddModelError(nameof(appointment.TermsAccepted), "You must accept the terms");
+            }
+
+            if (ModelState.IsValid)
+                return View("Completed", appointment);
+            else
+                return View(appointment);
         }
     }
 }
