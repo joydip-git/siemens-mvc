57799ff668cf48f3afc36bbefa906ce3f1fd803e
siemens-c2c <joy_mon78@yahoo.co.in>
2019-09-25 16:05:06 +0530

    created database tables

---
+++ query.sql
@@ -0,0 +1,22 @@
+﻿--create database siemens_db
+
+--create table categories(
+--categoryid int not null primary key identity(1,1),
+--categorynme varchar(50) not null
+--)
+
+/*
+create table products(
+productid int not null primary key,
+productname varchar(50) not null,
+price decimal(18,2) default(0),
+description varchar(max)
+)
+
+alter table products
+add categoryid int
+
+alter table products
+add constraint fk_categories_products_categoryid 
+foreign key(categoryid) references categories(categoryid)
+*/
